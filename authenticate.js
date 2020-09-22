7b3bb6a2ea659d767f79915f285c0cc91215e69d
Naveen Hugar <naveenhugarnh21@gmail.com>
2020-09-22 22:18:37 +0530

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -35,3 +35,16 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin=(req,res,next) => { 
+    var isAdmin = req.user.admin; 
+    if (!isAdmin) { 
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        next(err);
+        return;
+    } 
+    next(); 
+}
+    
+    
