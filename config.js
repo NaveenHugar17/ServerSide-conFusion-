d15f4b2d3bccd470fdef1f9cdba4d4a722f8dbeb
Naveen Hugar <naveenhugarnh21@gmail.com>
2020-09-28 15:01:08 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '430929941208863',
+        clientSecret: 'd3322558f6e37b7006a9e7de2079710d'
+    }
 }
